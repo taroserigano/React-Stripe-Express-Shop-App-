5f1c83bc9a240b08d8b82f1387631472b452908a
Taro Serigano <taroserigano@gmail.com>
2022-08-31 18:41:49 -0500

    Update checkout.js

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,8 +1,12 @@
 const stripeAPI = require('../stripe');
 
-async function createCheckoutSession(req, res) {
+// just pick up line_items such as what bought and how much etc and then 
+// send that to stripe site with additional info like email etc 
+async function createCheckoutSession(req, res) 
+
   const domainUrl = process.env.WEB_APP_URL;
   const { line_items, customer_email } = req.body;
+
   // check req body has line items and email
   if (!line_items || !customer_email) {
     return res.status(400).json({ error: 'missing required session parameters' });
