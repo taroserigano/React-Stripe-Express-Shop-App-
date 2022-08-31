a10a00afdc2a81f65aafaca320627b63b655cfef
Taro Serigano <taroserigano@gmail.com>
2022-08-31 18:33:23 -0500

    Update webhook.js

--- server/api/webhook.js
+++ server/api/webhook.js
@@ -1,11 +1,13 @@
 const stripeAPI = require('../stripe');
 
+// when payment finished 
 const webHookHandlers = {
+  // when checkout  completed 
   'checkout.session.completed': (data) => {
     console.log('Checkout completed successfully', data);
     // other business logic
   },
-
+   // when payment went through  
   'payment_intent.succeeded': (data) => {
     console.log('Payment succeeded', data);
   },
@@ -14,17 +16,20 @@ const webHookHandlers = {
   }
 }
 
+// basically a logger 
 function webhook(req, res) {
   const sig = req.headers['stripe-signature'];
+  // transaction event 
   let event;
 
   try {
+    // rawBody is the whole data about what purchased, buyer address, total $ etc junk 
     event = stripeAPI.webhooks.constructEvent(
-      req['rawBody'], sig, process.env.WEB_HOOK_SECRET);
+      req['rawBody'], sig, process.env.WEB_HOOK_SECRET); // just a secret code for the web hook logger 
   } catch(error) {
     return res.status(400).send(`Webhook error ${error.message}`);
   }
-
+  // this handler process each case scenario of payment successed etc 
   if (webHookHandlers[event.type]) {
     webHookHandlers[event.type](event.data.object);
   }
